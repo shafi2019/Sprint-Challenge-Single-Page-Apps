4d31632f58bec624fdfcd11d75b744025ce4b44a
Shafi Masoumi <masoumi2017@icloud.com>
2019-10-12 11:09:09 -0700

    data Fetched from APi

--- src/App.js
+++ src/App.js
@@ -6,8 +6,36 @@ import CharacterList from "./components/CharacterList";
 import WelcomePage from "./components/WelcomePage.js";
 
 
+const HeaderStyle = styled.div`
+background-image: url(https://images.pexels.com/photos/2115578/pexels-photo-2115578.jpeg?cs=srgb&dl=adventure-blue-sky-boat-2115578.jpg&fm=jpg);
+`
+
+const NavContentStyle = styled.div`
+display: flex;
+justify-content: center;
+text-align: justify;
+margin-top: 2.5%;
+`
+
+
 export default function App() {
   return (
-
+    <HeaderStyle>
+    <main>
+    <Header />
+    <Route exact path="/" component={WelcomePage}/> 
+    <Route exact path="/CharacterList" component={CharacterList}/> 
+    <NavContentStyle>
+    <div className="App">
+        <div>
+        <Link to="/">Home</Link>
+        </div>
+        <div>
+        <Link to="/characterList">Character List</Link>
+        </div>
+    </div>
+    </NavContentStyle> 
+    </main>
+    </HeaderStyle>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -4,15 +4,47 @@ import styled from 'styled-components';
 import CharacterCard from "./CharacterCard";
 import SearchForm from "./SearchForm";
 
+const CharacterListStyling = styled.div`
+background-color: #F4A89F;
+display: flex;
+justify-content: center;
+`
+
 export default function CharacterList() {
 
-
+  const [Character, setCharacter] = useState([]);
+  const [initialCharacter, setInitialCharacter] = useState([]);
   
   useEffect(() => {
+
+      axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(response  => {
+      setCharacter(response.data.results)
+  
+      setInitialCharacter(response.data.results)
   
       }, [])
+  
+      .catch(error => {
+          console.log("Error", error);
+      })
+    }, []);
+
+
 
   return (
-
+    <CharacterListStyling>
+    <section className="character-list">
+      {Character.map((character) =>
+        <CharacterCard
+          key={character.id}
+          name={character.name}
+          status={character.status}
+          image={character.image}
+          />
+          )}
+    </section>
+    </CharacterListStyling>
   );
 }
